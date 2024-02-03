7147dcb0e0dbdc5c0df1db65d19ad533903a6c51
Meshack <meshack.odunowe@decagon.dev>
2024-02-03 22:56:25 +0100

    removed token

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,5 +7,5 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
-  token:'sknYcBgjIsBOtSB5SpqeTJtaaZexJZL0OUoXRgRWAHzCVxPwklDquvuGQjOOgqfTZDJu6XOVZUUihUqULvVe9qUjmCiLhc5LFj4FyqPGWkl9uouozbP44sKCJpYimZrrcWPIPJmIqkncs8QeHkNlNGrwaJrdNOQ9FaFlbMU9qqtWC5b0WZHf',
+  
 })
